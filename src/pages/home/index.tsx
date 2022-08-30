ea0f9b6a7b1d2f17ab4f915f81aa2b105b3499ed
atiladefreitas <atila.defreitas@icloud.com>
2022-08-30 15:42:15 -0300

    first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "./styles";
+
+function Home():JSX.Element {
+ return (
+  <Container>
+    <h1>hello</h1>
+  </Container>
+ )  
+}
+
+export { Home }
