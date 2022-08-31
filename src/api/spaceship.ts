3f291dd4c0a0452d4a6e432917a08df2e443e1a4
atiladefreitas <atila.defreitas@icloud.com>
2022-08-30 22:36:12 -0300

    created .env file

---
+++ src/api/spaceship.ts
@@ -0,0 +1,13 @@
+export async function FetchSpaceships(){
+  const url = import.meta.env.VITE_API_URL;
+
+  console.log('katchau', url)
+
+  const response = await fetch(`${url}/starships/`);
+  
+  if (response.status === 200) {
+    return await response.json();
+  } else {
+    return null
+  }
+}
